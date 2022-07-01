406c8ac24050b801cecdb16a3a3da6fa3c120405
Luana Chaves <luanacsiqueira@hotmail.com.br>
2022-07-01 15:12:09 -0300

    feat: update to apply the private modifier to set function of cpf properties

--- digitalinnovation-main/fundamentos_oo/src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ digitalinnovation-main/fundamentos_oo/src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -1,7 +1,14 @@
 package one.digitalinnovation.digionebank
 
-abstract class Pessoa(
-    val nome: String,
-    val cpf: String
-)
+class Pessoa{
+    var nome: String = "Luana"
+    var cpf: String = "123.123.123.11"
+    private set
+}
+fun main(){
+    val luana = Pessoa()
 
+    println(luana.nome)
+    println(luana.cpf)
+
+}
