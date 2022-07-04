679d0510f5418f4392dcc458a489d258a5474776
Luana Chaves <luanacsiqueira@hotmail.com.br>
2022-07-04 12:10:10 -0300

    feat: implementando a interface logavel e a classe teste autenticação

--- digitalinnovation-main/fundamentos_oo/src/main/kotlin/one/digitalinnovation/digionebank/testes/TesteFuncionarioGerente.kt
+++ digitalinnovation-main/fundamentos_oo/src/main/kotlin/one/digitalinnovation/digionebank/testes/TesteFuncionarioGerente.kt
@@ -6,6 +6,7 @@ import one.digitalinnovation.digionebank.Gerente
 import java.math.BigDecimal
 
 fun main(){
-    val maria = Gerente("Maria Fulano", "123.123.123.12",5000.0)
+    val maria = Gerente("Maria Fulano", "123.123.123.12",5000.0, "senha123")
     ImprimeRelatorioFuncionario.imprime(maria)
+    TesteAutenticacao().autentica(maria)
 }
